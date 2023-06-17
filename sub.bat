2ac3817b8fb96f03d8aaa6cab969d105b86077ca
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-06-17 22:59:45 +0100

    Not needed

--- sub.bat
+++
@@ -1,2 +0,0 @@
-@REM Crunchyroll only!
-@yt --sub-lang enUS --write-sub --embed-subs -o "~\Videos\Shows\%%(series)s\%%(season_number|)s %%(season|)s %%(episode_number)02d - %%(episode)s.%%(ext)s" --cookies "~\crunchycookies.txt" %*
--- tw.bat
+++
@@ -1,1 +0,0 @@
-@yt -o "~\Videos\Streams\%%(uploader)s\%%(timestamp>%%Y-%%m-%%d-%%H-%%M-%%S,upload_date>%%Y-%%m-%%d-%%H-%%M-%%S|Unknown)s %%(title)s.%%(ext)s" %*
